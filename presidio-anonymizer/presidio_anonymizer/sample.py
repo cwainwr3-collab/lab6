ca880b9f57d738342c61a19cde67b861052b9301
Christopher Wainwright <231749634+cwainwr3-collab@users.noreply.github.com>
2025-10-02 02:57:20 +0000

    trying to fix return on sample.py

--- presidio-anonymizer/presidio_anonymizer/sample.py
+++ presidio-anonymizer/presidio_anonymizer/sample.py
@@ -14,8 +14,7 @@ def sample_run_anonymizer(text : str, start : int, end : int):
         analyzer_results=[RecognizerResult(entity_type="PERSON", start=start, end=end)],
         operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})}
     )
-
-    return (result)
+    return result
 
 if __name__ == "__main__": 
     anonymizer_ex = sample_run_anonymizer("My name is Bond.", 11, 15)
