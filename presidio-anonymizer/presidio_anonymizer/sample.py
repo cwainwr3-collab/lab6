b4807f7d4e04ef924535ecc37624c098e2a08e35
Christopher Wainwright <231749634+cwainwr3-collab@users.noreply.github.com>
2025-10-02 02:47:11 +0000

    Trying to fix function signature and supposed missing return statement

--- presidio-anonymizer/presidio_anonymizer/sample.py
+++ presidio-anonymizer/presidio_anonymizer/sample.py
@@ -1,7 +1,7 @@
 from presidio_anonymizer import AnonymizerEngine
 from presidio_anonymizer.entities import RecognizerResult, OperatorConfig
 
-def sample_run_anonymizer(textstr: str, starti: int, endi: int):
+def sample_run_anonymizer(textstr: str, starti: int, endi: int, scorei: float):
     # Initialize the engine
     engine = AnonymizerEngine()
 
@@ -11,27 +11,12 @@ def sample_run_anonymizer(textstr: str, starti: int, endi: int):
     result = engine.anonymize(
         ##text=input("text: "),
         text = textstr,
-        analyzer_results=[RecognizerResult(entity_type="PERSON", start=starti, end=endi, score=0.8)],
+        analyzer_results=[RecognizerResult(entity_type="PERSON", start=starti, end=endi, score=scorei)],
         operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})}
     )
-
-    # this print is being changed to return a proper testable value
-    # print(result)
-
-    # input should be:
-    # text: My name is Bond.
-    # start: 11
-    # end: 15
-    # 
-    # output should be:
-    # text: My name is BIP.
-    # items:
-    # [
-    #     {'start': 11, 'end': 14, 'entity_type': 'PERSON', 'text': 'BIP', 'operator': 'replace'}
-    # ]
 
     return (result)
 
 if __name__ == "__main__": 
-    anonymizer_ex = sample_run_anonymizer("My name is Bond.", 11, 15)
+    anonymizer_ex = sample_run_anonymizer("My name is Bond.", 11, 15, .8)
     print(anonymizer_ex)
